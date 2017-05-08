346c7437137e1e6c12a0f0b5062c5896d4d8d8e9
Jamie Pottage <pottage7@gmail.com>
2017-05-09 00:10:08 +0100

    Put temp scripts into main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ $('body').click('a[href="#"]', function (event) {
 $('.navMobileToggle').click(function () {
     $('.be-head .navHov').toggleClass('toggled').find('.fa').toggleClass('fa-caret-down fa-caret-up');
 });
+
+// hide recruitment table rows if not recruiting that class
+$('tr:has("td:empty")').hide();
+
+// styles progression based on cell data
+$('td:contains("Mythic")').addClass('kill-mythic');
+$('td:contains("Heroic")').addClass('kill-heroic');
+$('td:contains("Normal")').addClass('kill-normal');
