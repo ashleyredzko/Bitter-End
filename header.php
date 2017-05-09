6767c8fe060d72ee16fd5d85227c29f11f355671
Jamie Pottage <pottage7@gmail.com>
2017-05-09 01:15:43 +0100

    Implemented php files to include header and footer
    Finished recruitment page, links need doing but will finish later.

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<script src="https://use.fontawesome.com/15ecff4722.js"></script>
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	    <link rel="stylesheet" href="css/style.css">
+
+	    <style>
+	    	tr:nth-of-type(odd) {
+				background-color:#272727;
+	    	}
+		</style>
+
+		<title>Bitter End | Illidan US</title>
+	</head>
+	<body>
+
+		<!-- Header -->
+		<nav class="navbar be-head">
+			<div class="be-logo"></div>
+			<div class="navHov">
+				<a href="#" class="navMobileToggle hidden-sm-up">MENU <i class='fa fa-caret-down'></i></a>
+				<div class="mainNav">
+					<a href="index.php" >HOME</a>
+					<a href="recruitment.php" >RECRUITMENT</a>
+					<a href="media.php" >MEDIA</a>
+					<a href="contact.php" >CONTACT</a>
+				</div>
+			</div>
+		</nav>
---
+++ recruitment.php
@@ -0,0 +1,288 @@
+<?php
+	require('header.php');
+?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 highlight-panel highlight-img">
+			<div class="highlight-wrap">
+				<div class="highlight-title">
+				Mythic Star Augur Etraeus Dead
+				</div>
+				<div class="highlight-desc">
+				Rumor has it he saw stars...
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-3 be-sidebar">
+			<p class="sidebar-section"></span>Recruitment</p>
+			<button type="button" class="btn btn-success btn-sm btn-block">Apply</button>
+			<div class="side-recruitment">
+				<p>We recruit for immediate positions and don't believe in recruiting for a bench. If your application is successful you will see considerable raid time.</p>
+				<table class="recruit-table">
+					<tr class="status-dk">
+						<td class="class-dk"><img src="img/class_icons/class-dk.jpg" />Death Knight</td>
+						<td class="rec-need-dk pull-right"></td>
+					</tr>
+					<tr class="status-dh">
+						<td class="class-dh"><img src="img/class_icons/class-dh.jpg" />Demon Hunter</td>
+						<td class="rec-need-dh pull-right"></td>
+					</tr>
+					<tr class="status-druid">
+						<td class="class-druid"><img src="img/class_icons/class-druid.jpg" />Druid</td>
+						<td class="rec-need-druid pull-right"></td>
+					</tr>
+					<tr class="status-hunter">
+						<td class="class-hunter"><img src="img/class_icons/class-hunter.jpg" />Hunter</td>
+						<td class="rec-need-hunter pull-right">Marks/BM</td>
+					</tr>
+					<tr class="status-mage">
+						<td class="class-mage"><img src="img/class_icons/class-mage.jpg" />Mage</td>
+						<td class="rec-need-mage pull-right">Frost/Arcane</td>
+					</tr>
+					<tr class="status-monk">
+						<td class="class-monk"><img src="img/class_icons/class-monk.jpg" />Monk</td>
+						<td class="rec-need-monk pull-right"></td>
+					</tr>
+					<tr class="status-paladin">
+						<td class="class-paladin"><img src="img/class_icons/class-paladin.jpg" />Paladin</td>
+						<td class="rec-need-paladin pull-right"></td>
+					</tr>
+					<tr class="status-priest">
+						<td class="class-priest"><img src="img/class_icons/class-priest.jpg" />Priest</td>
+						<td class="rec-need-priest pull-right">Shadow</td>
+					</tr>
+					<tr class="status-rogue">
+						<td class="class-rogue"><img src="img/class_icons/class-rogue.jpg" />Rogue</td>
+						<td class="rec-need-rogue pull-right"></td>
+					</tr>
+					<tr class="status-shaman">
+						<td class="class-shaman"><img src="img/class_icons/class-shaman.jpg" />Shaman</td>
+						<td class="rec-need-shaman pull-right"></td>
+					</tr>
+					<tr class="status-warlock">
+						<td class="class-warlock"><img src="img/class_icons/class-warlock.jpg" />Warlock</td>
+						<td class="rec-need-warlock pull-right">Any</td>
+					</tr>
+					<tr class="status-warrior">
+						<td class="class-warrior"><img src="img/class_icons/class-warrior.jpg" />Warrior</td>
+						<td class="rec-need-warrior pull-right"></td>
+					</tr>
+				</table>
+			</div>
+			<p class="sidebar-section"></span>Progression</p>
+			<button type="button" class="btn btn-primary btn-sm btn-block">WoWProgress</button>
+			<button type="button" class="btn btn-warning btn-sm btn-block">Warcraft Logs</button>
+			<div class="side-progression">
+
+				<button data-toggle="collapse" data-target="#prog-nh" class="prog-nh-btn"><span>The Nighthold</span></button>
+
+					<div id="prog-nh" class="collapse in">
+						<div class="prog-col-content">
+							<table class="prog-table">
+								<tr>
+									<td>Skorpyron</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Chronomatic Anomaly</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Trilliax</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Spellblade Aluriel</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Tichondrius</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Krosus</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>High Botanist Tel'arn</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Star Augur Etraeus</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Grand Magistrix Elisande</td>
+									<td class="pull-right kill-status">Heroic</td>
+								</tr>
+								<tr>
+									<td>Gul'dan</td>
+									<td class="pull-right kill-status">Heroic</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+
+				<button data-toggle="collapse" data-target="#prog-tov" class="prog-tov-btn"><span>Trial of Valor</span></button>
+
+					<div id="prog-tov" class="collapse">
+						<div class="prog-col-content">
+							<table class="prog-table">
+								<tr>
+									<td>Odyn</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Guarm</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Helya</td>
+									<td class="pull-right kill-status">Heroic</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+
+				<button data-toggle="collapse" data-target="#prog-en" class="prog-en-btn"><span>Emerald Nightmare</span></button>
+
+					<div id="prog-en" class="collapse">
+						<div class="prog-col-content">
+							<table class="prog-table">
+								<tr>
+									<td>Nythendra</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Il'gynoth, Heart of Corruption</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Elerethe Renferal</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Ursoc</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Dragons of Nightmare</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+								<tr>
+									<td>Cenarius</td>
+									<td class="pull-right kill-status">Mythic</td
+								></tr>
+								<tr>
+									<td>Xavius</td>
+									<td class="pull-right kill-status">Mythic</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				
+			</div>
+		</div>
+		<div class="col-md-9 be-news">
+			<p class="news-title">Bitter End Recruitment</p>
+			<div class="news-content">
+				<p>Please view the Recruitment section on the sidebar to see our current class/spec needs, and before applying please read the following information. All times stated are Illidan server time (Central US).</p>
+				<p class="news-title">Raid Schedule</p>
+				<table class="raid-sched">
+					<tr>
+						<th>Day</th>
+						<th>Start</th>
+						<th>End</th>
+					</tr>
+					<tr>
+						<td>Tuesday</td>
+						<td>21:00</td>
+						<td>00:00</td>
+					</tr>
+					<tr>
+						<td>Wednesday</td>
+						<td>21:00</td>
+						<td>00:00</td>
+					</tr>
+					<tr>
+						<td>Thursday</td>
+						<td>21:00</td>
+						<td>00:00</td>
+					</tr>
+					<tr>
+						<td>Sunday (casual/alt)</td>
+						<td>21:00</td>
+						<td>00:00</td>
+					</tr>
+				</table>
+
+				<p>Raids typically begin forming 20 minutes before stated time. All raiders are expected to be in raid and at the entrance 10 minutes before hand accompanied by their flasks, food, pots, runes and any other consumables the raid night may call for.</p>
+
+				<p class="news-title">About us</p>
+
+				<ul>
+					<li>Bitter End is a close knit family of players here to build long lasting friendships. We're very fun, open-hearted and full of banter and are looking for people with similar personalities.</li>
+					<li>While we are very relaxed during normal play, we take raiding fairly seriously and won't accept lazy and sub-optimal play. We're not expecting players to be parsing #1, we simply want people to be prepared, knowledgeable about their class and every encounter, and be open to constructive criticism. We value people who play to benefit the team and not always themselves.</li>
+					<li>The trial period typically lasts 3 weeks, at the end of which you will either pass, fail or be extended by 2 weeks. During this time will you be judged on your attendance, attitude and performance.</li>
+					<li>We handle loot as a council of experienced players. We base our decisions mainly on the benefit to the raid team, but take into consideration attendance and performance. We do not favor officers over raiders, however we may favor members over trials for a short period time to prevent people using our guild as a stepping stone to another guild. We seek to recruit long lasting and loyal members and aren't looking for players whose sole concern is obtaining loot.</li>
+					<li>We reward and invest in loyal players. Sticking through the hard times never go unnoticed.</li>
+				</ul>
+
+				<p class="news-title">You should apply if</p>
+
+				<ul>
+					<li>You have a respectable background of raiding throughout multiple expansions and tiers.</li>
+					<li>You have the mindset of a mythic raider. If wiping on the same boss for 9 hours a week seems daunting to you, perhaps look elsewhere.</li>
+					<li>You're looking for a guild to stay in for a long time, and you're not the type of person to guild hop.</li>
+					<li>You're looking for a mythic raiding team without the added stress angry raid leaders and toxic environments.</li>
+					<li>You possess the initiative to research new raids/encounters, and keep up to date with your class/spec theorycrafting; constantly looking for ways to improve yourself and your play.</li>
+				</ul>
+
+				<p class="news-title">Who to contact if I have questions?</p>
+
+				<table class="raid-sched">
+					<tr>
+						<th>Name</th>
+						<th>Rank</th>
+						<th>BTag</th>
+					</tr>
+					<tr>
+						<td>Thetolltroll</td>
+						<td>Officer</td>
+						<td>troll#1663</td>
+					</tr>
+					<tr>
+						<td>Redseries</td>
+						<td>Officer</td>
+						<td>Redseries#1591</td>
+					</tr>
+					<tr>
+						<td>Fooshe</td>
+						<td>Officer</td>
+						<td>Foosh#1338</td>
+					</tr>
+					<tr>
+						<td>Panthèon</td>
+						<td>Guild Master</td>
+						<td>pantheon#2629</td>
+					</tr>
+				</table>
+
+				<p class="news-title">Sounds good, now what?</p>
+
+				<button type="button" class="btn btn-success btn-lg btn-block">Apply!</button>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+	require('footer.php');
+?>
