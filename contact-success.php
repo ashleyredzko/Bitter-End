b24bed61a2520a0bba0c1bb0717e94e2b17eb2fc
Jamie Pottage <pottage7@gmail.com>
2017-05-11 18:23:30 +0100

    Last commit only commited half of the contact-success.php file ????

--- contact-success.php
+++ contact-success.php
@@ -1,6 +1,13 @@
 <?php 
 
+	$name		= $_POST['ct-name'];
+	$reply		= $_POST['ct-btagemail'];
+	$about		= $_POST['ct-about'];
+	$msg		= $_POST['ct-msg'];
 
+	$to			= "";
+	$subject	= "";
+	$body		= "";
 
 	mail($to,$subject,$body);
 
