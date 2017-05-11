e218ad264df28014f130a1b932455aeec5bf3beb
Jamie Pottage <pottage7@gmail.com>
2017-05-12 00:28:00 +0100

    Finished contact form backend.

--- contact-success.php
+++ contact-success.php
@@ -6,8 +6,8 @@
 	$msg		= $_POST['ct-msg'];
 
 	$to			= "";
-	$subject	= "";
-	$body		= "";
+	$subject	= "Bitter End mail from: $name regarding $about";
+	$body		= "$name has sent an email from the guild website.\n\nTheir information:\n\nName: $name\nEmail/BTag: $reply\nReason for contacting: $about\n\nTheir message:\n$msg";
 
 	mail($to,$subject,$body);
 
