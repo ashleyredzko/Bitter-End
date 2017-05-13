494d56fe8d19320f1f526eb97b39c596d3a32aac
Tyler Redzko <tylerredzko@gmail.com>
2017-05-13 02:11:34 -0400

    Remove now-unneeded js recruitment hiding

--- js/main.js
+++ js/main.js
@@ -7,9 +7,6 @@ $('.navMobileToggle').click(function () {
     $('.be-head .navHov').toggleClass('toggled').find('.fa').toggleClass('fa-caret-down fa-caret-up');
 });
 
-// hide recruitment table rows if not recruiting that class
-$('tr:has("td:empty")').hide();
-
 // styles progression based on cell data
 $('td:contains("Mythic")').addClass('kill-mythic');
 $('td:contains("Heroic")').addClass('kill-heroic');
