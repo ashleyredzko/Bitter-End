34a9c01cb139adfce38b42a58f35fc414ef9b83e
Tyler Redzko <tylerredzko@gmail.com>
2017-05-14 17:02:50 -0400

    Add sticky footer
    Clean up styling, reword some of the information links to avoid having to
    word-wrap or have awkward styling on mobile.

--- footer.php
+++ footer.php
@@ -1,48 +1,46 @@
-<div class="container-flex">
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3">
-					<div class="be-logo"></div>
+		<footer class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-3 hidden-sm-down">
+						<div class="be-logo"></div>
+					</div>
+					<div class="col-4 col-md-3">
+						<p>Navigation</p>
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Recruitment</a></li>
+							<li><a href="#">Media</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>
+					<div class="col-4 col-md-3">
+						<p>Links</p>
+						<ul>
+							<li><a href="#">WoWProgress</a></li>
+							<li><a href="#">Warcraft Logs</a></li>
+							<li><a href="#">YouTube</a></li>
+							<li><a href="#">Twitch</a></li>
+						</ul>
+					</div>
+					<div class="col-4 col-md-3">
+						<p>Information</p>
+						<ul>
+							<li><a href="#">Sales</a></li>
+							<li><a href="#">Application</a></li>
+							<li><a href="#">Leadership</a></li>
+							<li><a href="#">Admin</a></li>
+						</ul>
+					</div>
 				</div>
-				<div class="col-md-3 panel">
-					<p>Navigation</p>
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Recruitment</a></li>
-						<li><a href="#">Media</a></li>
-						<li><a href="#">Contact</a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 panel">
-					<p>Links</p>
-					<ul>
-						<li><a href="#">WoWProgress</a></li>
-						<li><a href="#">Warcraft Logs</a></li>
-						<li><a href="#">YouTube</a></li>
-						<li><a href="#">Twitch</a></li>
-					</ul>
-				</div>
-				<div class="col-md-3 panel">
-					<p>Information</p>
-					<ul>
-						<li><a href="#">Sales Information</a></li>
-						<li><a href="#">Application</a></li>
-						<li><a href="#">Leadership List</a></li>
-						<li><a href="#">Admin Panel</a></li>
-					</ul>
+				<div class="row">
+					<div class="col-12">
+						<p class="text-center">&copy; 2017 Bitter End. All Rights Reserved.</p>
+					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<p class="text-center">&copy; 2017 Bitter End. All Rights Reserved.</p>
-				</div>
-			</div>
-		</div>
-	</footer>
-</div>
+		</footer>
 
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 		<script src="js/main.js"></script>
